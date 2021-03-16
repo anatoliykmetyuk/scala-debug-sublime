72da3b6fb3dc00df6d91c5a82571b77a2e700ed8
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2021-03-17 00:21:31 +0100

    Configs and settings aggregated

--- ScalaDebug.py
+++ ScalaDebug.py
@@ -3,7 +3,7 @@ import sublime_plugin
 
 class ScalaDebugCommand(sublime_plugin.TextCommand):
   def run(self, edit, snippet):
-    target_snippet = sublime.load_settings("ScalaDebug.sublime-settings").get("snippets")[snippet]
+    target_snippet = sublime.load_settings("Default.sublime-settings").get("scala_debug")['snippets'][snippet]
     for r in self.view.sel():
       text = self.view.substr(r)
       self.view.replace(edit, r, target_snippet.format(text))
